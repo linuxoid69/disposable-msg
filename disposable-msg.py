907c7b8aafb6714f94c79ef51aaa28c963143816
rustam <rustam@my-itclub.ru>
2016-01-14 23:42:35 +0300

    change style

--- disposable-msg.py
+++ disposable-msg.py
@@ -16,19 +16,25 @@ def index():
     idsession = uuid.uuid4()
     pw = md5(str(idsession))
     passwd = pw.hexdigest()
+
     if request.method == 'POST':
        if (request.form['message']):
           genlib.write_message(passwd, idsession, request.form['message'])
+
        return render_template('form.html', link='%s?idsession=%s&passwd=%s' % (request.base_url, idsession, passwd))
 
     if (request.args.get('idsession', '') and  request.args.get('passwd', '')):
         ids = request.args.get('idsession', '')
         pss = request.args.get('passwd', '')
         message = genlib.read_message(pss, ids)
+
         if message == None:
             return render_template('page404.html')
+        
         genlib.delete_message(pss, ids)
+        
         return render_template('form.html', message=message)
+        
     return render_template('form.html')
 
 
